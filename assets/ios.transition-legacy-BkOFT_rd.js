bcaaf35b0d4efb0a23b27739e3c29589c7b7a5d7
20221936clint <20221936@nbsc.edu.ph>
2025-05-27 23:55:14 +0800

    Updates

--- assets/ios.transition-legacy-BkOFT_rd.js
+++ assets/ios.transition-legacy-BkOFT_rd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7xdo2Lh.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-bAZMOFTF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
